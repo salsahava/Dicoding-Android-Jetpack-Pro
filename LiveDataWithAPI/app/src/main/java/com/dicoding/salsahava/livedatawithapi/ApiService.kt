381c07440f402dab59cec9832fafffc1fd2dbb9f
salsahava <1shavaqabitta@gmail.com>
2021-05-08 16:05:24 +0700

    LiveData with API (Mengambil Data Review Restoran)

---
+++ LiveDataWithAPI/app/src/main/java/com/dicoding/salsahava/livedatawithapi/ApiService.kt
@@ -0,0 +1,12 @@
+package com.dicoding.salsahava.livedatawithapi
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface ApiService {
+    @GET("detail/{id}")
+    fun getRestaurant(
+        @Path("id") id: String
+    ): Call<RestaurantResponse>
+}
---
+++ LiveDataWithAPI/app/src/main/java/com/dicoding/salsahava/livedatawithapi/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.dicoding.salsahava.livedatawithapi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.ArrayAdapter
+import androidx.activity.viewModels
+import com.bumptech.glide.Glide
+import com.dicoding.salsahava.livedatawithapi.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val mainViewModel: MainViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
+
+        supportActionBar?.hide()
+
+        mainViewModel.restaurant.observe(this, { restaurant ->
+            activityMainBinding.apply {
+                tvTitle.text = restaurant.name
+                tvDescription.text = restaurant.description
+
+                Glide.with(this@MainActivity)
+                    .load("https://restaurant-api.dicoding.dev/images/large/${restaurant.pictureId}")
+                    .into(ivPicture)
+            }
+        })
+
+        mainViewModel.listReview.observe(this, { consumerReviews ->
+            val listReview = consumerReviews.map {
+                "${it.review}\n- ${it.name}"
+            }
+
+            activityMainBinding.apply {
+                lvReview.adapter = ArrayAdapter(this@MainActivity, R.layout.item_review, listReview)
+                edReview.setText("")
+            }
+        })
+
+        mainViewModel.isLoading.observe(this, {
+            activityMainBinding.progressBar.visibility = if (it) View.VISIBLE else View.GONE
+        })
+    }
+}
