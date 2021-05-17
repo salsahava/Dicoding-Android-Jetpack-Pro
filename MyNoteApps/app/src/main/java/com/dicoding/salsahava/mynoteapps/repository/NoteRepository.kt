ebbd0b2c855c00e43715cad90e566d91d62d4e8e
salsahava <1shavaqabitta@gmail.com>
2021-05-18 01:36:13 +0700

    Room

---
+++ MyNoteApps/app/src/main/java/com/dicoding/salsahava/mynoteapps/repository/NoteRepository.kt
@@ -0,0 +1,39 @@
+package com.dicoding.salsahava.mynoteapps.repository
+
+import android.app.Application
+import androidx.lifecycle.LiveData
+import com.dicoding.salsahava.mynoteapps.database.Note
+import com.dicoding.salsahava.mynoteapps.database.NoteDao
+import com.dicoding.salsahava.mynoteapps.database.NoteRoomDatabase
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+
+class NoteRepository(application: Application) {
+    private val mNotesDao: NoteDao
+    private val executorService: ExecutorService = Executors.newSingleThreadExecutor()
+
+    init {
+        val db = NoteRoomDatabase.getDatabase(application)
+        mNotesDao = db.noteDao()
+    }
+
+    fun getAllNotes(): LiveData<List<Note>> = mNotesDao.getAllNotes()
+
+    fun insert(note: Note) {
+        executorService.execute {
+            mNotesDao.insert(note)
+        }
+    }
+
+    fun delete(note: Note) {
+        executorService.execute {
+            mNotesDao.delete(note)
+        }
+    }
+
+    fun update(note: Note) {
+        executorService.execute {
+            mNotesDao.update(note)
+        }
+    }
+}
